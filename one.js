40830a070a7fa9daf48363238c52dd70f4d70d8f
Darwin-willware <144683078+Darwin-willware@users.noreply.github.com>
2023-09-11 20:35:10 +0530

    v3

--- one.js
+++ one.js
@@ -16,8 +16,33 @@ let percentage = 0;
     console.log("ctc is"+ "  "+ this.tds);
 
     document.getElementById("tds").innerHTML = this.tds;
-    Math.round(this.tds);
-    console.log(this.tds);
+    const getlastthree = String(this.tds).slice(-3);
+    let lastdigits = Number(getlastthree);
+    let parsectc = parseInt(ctc);
+    console.log("parsectc =>"+ parsectc);
+    let roundoff = String(parsectc).slice(0,-3);
+    let needToAdd = 0;
+    console.log( "ROUND OFF"+roundoff);
+
+    console.log(lastdigits);
+     if(lastdigits === 0 || lastdigits === 500){
+      document.getElementById("roundoff").innerHTML = this.tds;    
+    }else if(lastdigits<500){
+      this.lastdigits=500;
+      roundoff= roundoff+500;
+      console.log(roundoff);
+      document.getElementById("roundoff").innerHTML = parseInt(roundoff).toLocaleString("en-us");
+     }
+    else{
+          needToAdd = 1000-lastdigits;
+          console.log("ami =>"+needToAdd);
+          roundoff = parseInt(roundoff);
+          roundoff = parsectc + needToAdd;
+          console.log(roundoff);
+          document.getElementById("roundoff").innerHTML = roundoff.toLocaleString("en-us");
+    }
+    console.log(this.lastdigits);
+    console.log( String(this.tds).slice(-3));
 }
  function calculatehike(){
 //ctc/100 * percentage
