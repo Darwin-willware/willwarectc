30c2b471284fe3ecc8e2e8b5f189bc18a39a3b88
Darwin-willware <144683078+Darwin-willware@users.noreply.github.com>
2023-09-11 17:17:40 +0530

    v2
    ctc+hike

--- one.js
+++ one.js
@@ -1,5 +1,8 @@
 let takehome = 0;
 let tds =0;
+let newctc =0;
+let hikeamount = 0;
+let percentage = 0;
  function calculatectc(){
     let ctc= 0;
     
@@ -11,5 +14,21 @@ let tds =0;
     console.log(ctc)
     this.tds =( parseInt(ctc)).toLocaleString("en-us");
     console.log("ctc is"+ "  "+ this.tds);
+
     document.getElementById("tds").innerHTML = this.tds;
+    Math.round(this.tds);
+    console.log(this.tds);
 }
+ function calculatehike(){
+//ctc/100 * percentage
+let oldctc =0;
+oldctc = parseInt(document.getElementById("ctc").value);
+let percentage = parseInt(document.getElementById("percentage").value);
+console.log(oldctc,percentage);
+  this. hikeamount = (oldctc/100)*percentage;
+  this.newctc = oldctc+ this.hikeamount;
+   console.log(this.newctc);
+   document.getElementById("hikeamount").innerHTML = this.hikeamount;
+   document.getElementById("newctc").innerHTML = this.newctc;
+
+ }
