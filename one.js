4353beb48693ee547ea3c2ae1faf281d7f735044
Darwin-willware <144683078+Darwin-willware@users.noreply.github.com>
2023-09-12 13:06:32 +0530

    v5

--- one.js
+++ one.js
@@ -3,57 +3,72 @@ let tds =0;
 let newctc =0;
 let hikeamount = 0;
 let percentage = 0;
+let candidateName = '';
+let recruiterName ='';
+
  function calculatectc(){
     let ctc= 0;
-    
+    //getting values from html page
  takehome = parseInt(document.getElementById("takehome").value);
- console.log(takehome);
- console.log("from no where")
-         
+ candidateName = document.getElementById("candidatename").value;
+ recruiterName = document.getElementById("recruitername").value;
+ console.log(candidateName,recruiterName);
+         //calculate overall ctc = takehome*100 /90
     ctc = parseInt((100*takehome)/90) ;
-    console.log(ctc)
     this.tds =( parseInt(ctc)).toLocaleString("en-us");
-    console.log("ctc is"+ "  "+ this.tds);
-
+     //pushing value to the html
     document.getElementById("tds").innerHTML = this.tds;
+    //getting last three digits to round off
     const getlastthree = String(this.tds).slice(-3);
     let lastdigits = Number(getlastthree);
     let parsectc = parseInt(ctc);
-    console.log("parsectc =>"+ parsectc);
     let roundoff = String(parsectc).slice(0,-3);
     let needToAdd = 0;
-    console.log( "ROUND OFF"+roundoff);
-
-    console.log(lastdigits);
      if(lastdigits === 0 || lastdigits === 500){
       document.getElementById("roundoff").innerHTML = this.tds;    
     }else if(lastdigits<500){
       this.lastdigits=500;
       roundoff= roundoff+500;
-      console.log(roundoff);
       document.getElementById("roundoff").innerHTML = parseInt(roundoff).toLocaleString("en-us");
      }
     else{
           needToAdd = 1000-lastdigits;
-          console.log("ami =>"+needToAdd);
           roundoff = parseInt(roundoff);
           roundoff = parsectc + needToAdd;
-          console.log(roundoff);
           document.getElementById("roundoff").innerHTML = roundoff.toLocaleString("en-us");
     }
-    console.log(this.lastdigits);
-    console.log( String(this.tds).slice(-3));
+    display(takehome,roundoff);
+
 }
  function calculatehike(){
 //ctc/100 * percentage
 let oldctc =0;
+let emphome = 0;
+let emptakehomenewctc = 0;
 oldctc = parseInt(document.getElementById("ctc").value);
 let percentage = parseInt(document.getElementById("percentage").value);
+
 console.log(oldctc,percentage);
+// calculating amount need to be hiked and total new ctc
   this. hikeamount = (oldctc/100)*percentage;
   this.newctc = oldctc+ this.hikeamount;
+  // calculating takehome from ctc
+  emphome = (90*oldctc)/100;
+  emptakehomenewctc =(90*this.newctc)/100;
+  console.log(emphome);
    console.log(this.newctc);
    document.getElementById("hikeamount").innerHTML = this.hikeamount;
    document.getElementById("newctc").innerHTML = this.newctc;
+   document.getElementById("oldtakehome").innerHTML = emphome;
+   document.getElementById("newtakehome").innerHTML = emptakehomenewctc;
+   
+
+ }
+ function display(takeh,ctcto){
+
+document.getElementById("cname").innerHTML = candidateName;
+document.getElementById("rname").innerHTML = recruiterName;
+document.getElementById("octc").innerHTML = ctcto;
+document.getElementById("thome").innerHTML = takeh;
 
  }
